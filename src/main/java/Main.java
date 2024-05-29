6885d85613b451358e4287fef6d667a1523b6b44
mac <taranenkovo03@gmail.com>
2024-05-30 00:22:11 +0300

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,15 @@ import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
-
-        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket;
         Socket clientSocket = null;
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
-            // Since the tester restarts your program quite often, setting SO_REUSEADDR
-            // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
-            // Wait for connection from client.
             clientSocket = serverSocket.accept();
-                readLinesFromClientAndSendResponse(clientSocket);
-
-
+            new RedisClient(clientSocket).run();
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
@@ -34,17 +26,5 @@ public class Main {
         }
     }
 
-    private static void readLinesFromClientAndSendResponse(Socket clientSocket) throws IOException {
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
-            OutputStream outputStream = clientSocket.getOutputStream();
-            String line;
-            while ((line = bufferedReader.readLine()) != null && !line.isEmpty() ) {
-                if (line.contains("PING")){
-                    outputStream.write("+PONG\r\n".getBytes());
-                    outputStream.flush();
-                }
 
-                }
-        }
-    }
 }
