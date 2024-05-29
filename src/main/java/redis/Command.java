c32d5d6cf82ec558a9264295c6c90c8eed8e8e70
mac <taranenkovo03@gmail.com>
2024-05-30 00:27:33 +0300

    commit

--- src/main/java/redis/Command.java
+++ src/main/java/redis/Command.java
@@ -1,8 +1,8 @@
 package redis;
 
 public enum Command {
-    PING("ping"),
-    PONG("pong");
+    PING("PING"),
+    PONG("PONG");
     private final String value;
     Command(String value) {
         this.value = value;
