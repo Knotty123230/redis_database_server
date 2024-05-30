d5cfe7ea19e35d2dbf3fd0878544ffbfd3c12ab1
mac <taranenkovo03@gmail.com>
2024-05-31 00:31:58 +0300

    commit

--- src/main/java/redis/command/EchoCommandProcessor.java
+++ src/main/java/redis/command/EchoCommandProcessor.java
@@ -5,6 +5,6 @@ import redis.Command;
 public class EchoCommandProcessor implements CommandProcessor {
     @Override
     public byte[] processCommand(String command) {
-        return ("$" + command.length() + command + "\r\n").getBytes();
+        return ("$" + command + "\r\n").getBytes();
     }
 }
