24ad8b01a75f64e9ff0a1761a3af02c044a21c61
mac <taranenkovo03@gmail.com>
2024-05-31 00:09:32 +0300

    commit

--- src/main/java/redis/RedisClient.java
+++ src/main/java/redis/RedisClient.java
@@ -20,23 +20,20 @@ public class RedisClient implements Runnable {
              InputStream inputStream = socket.getInputStream();
         ) {
             try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+                String command = "";
                 String line;
                 while ((line = bufferedReader.readLine()) != null && !line.isEmpty()) {
-                    boolean isValid = validateLine(line);
-                    if (!isValid) continue;
-                    byte[] bytes = processCommand(line);
-                    outputStream.write(bytes);
-                    outputStream.flush();
+                    command += line;
                 }
+                byte[] bytes = processCommand(command);
+                outputStream.write(bytes);
+                outputStream.flush();
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
     }
 
-    private boolean validateLine(String line) {
-        return line.equalsIgnoreCase(Command.ECHO.getValue()) || line.equalsIgnoreCase(Command.PING.getValue());
-    }
 
     private synchronized byte[] processCommand(String line) {
         CommandFactory commandFactory = new CommandFactory(line);
