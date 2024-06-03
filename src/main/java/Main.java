3006bd177b3ba49b2b3d5a77d21e2f84f98cbfd5
mac <taranenkovo03@gmail.com>
2024-06-03 23:31:06 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,21 @@ import redis.RedisClient;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+        Map<String, String> parameters = extractArgs(args);
         System.out.println("Logs from your program will appear here!");
-
         int port = 6379;
+        if (parameters.containsKey("--port")) {
+            port = Integer.parseInt(parameters.get("--port"));
+        }
         try (ServerSocket serverSocket = new ServerSocket(port);) {
             serverSocket.setReuseAddress(true);
             while (!serverSocket.isClosed()) {
@@ -22,6 +29,15 @@ public class Main {
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+    }
+
+    private static Map<String, String> extractArgs(String[] args) {
+        Map<String, String> parameters = new HashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            if (i + 1 >= args.length) break;
+            parameters.put(args[i], args[i + 1]);
+        }
+        return parameters;
     }
 
 
