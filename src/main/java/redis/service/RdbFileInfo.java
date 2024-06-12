75520d32a7e72820075330169d88c425dfdec3b3
mac <taranenkovo03@gmail.com>
2024-06-12 11:49:05 +0300

    codecrafters submit [skip ci]

--- src/main/java/redis/service/RdbFileInfo.java
+++ src/main/java/redis/service/RdbFileInfo.java
@@ -3,6 +3,7 @@ package redis.service;
 import redis.model.RdbFile;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,7 +15,7 @@ import java.util.stream.Stream;
 public class RdbFileInfo {
     private  RdbFile rdbFile;
     private static RdbFileInfo instance;
-
+    private File file;
     private RdbFileInfo() {
     }
 
@@ -27,8 +28,6 @@ public class RdbFileInfo {
     }
 
     public byte[] getContent() {
-        System.out.println(rdbFile.path() + rdbFile.fileName());
-        File file = new File(rdbFile.path() + rdbFile.fileName());
         byte[] decode;
         try (Stream<String> stringStream = Files.lines(Path.of(file.getPath()))) {
             String rdbFile = stringStream.collect(Collectors.joining());
@@ -54,5 +53,15 @@ public class RdbFileInfo {
              fileName = parameters.get("--dbfilename");
         }
         this.rdbFile = new RdbFile(path, fileName);
+        this.file = new File(rdbFile.path() + rdbFile.fileName());
+        if (!file.exists()){
+            file.mkdir();
+            try (FileWriter writer = new FileWriter(file)){
+                writer.write("UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog==");
+                writer.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
